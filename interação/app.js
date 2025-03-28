c007a4953a42b910843d53ff3a4b75f1918573ec
José Augusto Pereira Clarindo <AugustoPereira280102@gmail.com>
2025-03-28 20:09:52 -0300

    Update app.js
    Corrigindo a criptografia e descriptografia

--- interação/app.js
+++ interação/app.js
@@ -6,14 +6,14 @@ function criptografar(){
 
     var resultCripto = texto.replace(/e/g,"enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
-    document.getElementById('output').innerHTML = '<textarea readonly id"input-texto">'+resultCripto + '</textarea>' + '<button class="btn__copiar" id="copiar" onclick="copiar()">copiar</button>'
+    document.getElementById('output').innerHTML = '<textarea readonly id="input-texto">'+resultCripto + '</textarea>' + '<button class="btn__copiar" id="copiar" onclick="copiar()">copiar</button>'
 }
 function descriptografar(){
     var texto=textInput.value;
 
-    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufa/g, "ufat")
+    var resultDescripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufa/g, "u")
 
-    document.getElementById('output').innerHTML = `<textarea readonly id"input-texto">${resultDescripto}</textarea><button class="btn__copiar" id="copiar" onclick="copiar()">copiar</button>`
+    document.getElementById('output').innerHTML = `<textarea readonly id="input-texto">${resultDescripto}</textarea><button class="btn__copiar" id="copiar" onclick="copiar()">copiar</button>`
 }
 function copiar(){
     var textoCop= document.getElementById('output').querySelector('textarea');
