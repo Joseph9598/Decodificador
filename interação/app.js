8e80e38413570cc5ddc15d50f5bef0312a24e8fa
José Augusto Pereira Clarindo <AugustoPereira280102@gmail.com>
2025-03-28 20:24:52 -0300

    Update app.js

--- interação/app.js
+++ interação/app.js
@@ -11,11 +11,11 @@ function criptografar(){
 function descriptografar(){
     var texto=textInput.value;
 
-    var resultDescripto = texto.replace(/ufat/g, "u")  // Primeiro os maiores
-                               .replace(/ober/g, "o")
-                               .replace(/imes/g, "i")
-                               .replace(/enter/g, "e")
-                               .replace(/ai/g, "a")
+    var resultDescripto = texto.replace(/enter/g, "e")  // Processa "enter" primeiro
+                               .replace(/imes/g, "i")   // Depois "imes"
+                               .replace(/ai/g, "a")     // "ai" antes de "ufat"
+                               .replace(/ober/g, "o")   // "ober" depois
+                               .replace(/ufat/g, "u");  // "ufat" por último
 
     document.getElementById('output').innerHTML = `<textarea readonly id="input-texto">${resultDescripto}</textarea><button class="btn__copiar" id="copiar" onclick="copiar()">copiar</button>`
 }
